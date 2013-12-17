e6ef2b37d566294c367f8dc5d5b222649dd5288d
Julian Gruber <julian@juliangruber.com>
2013-12-17 15:56:53 +0100

    use juliangruber/find-urls

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var isUrl = require('regexps').url;
 var escape = require('escape-regexp');
 var Pressed = require('key-pressed');
+var findUrls = require('find-urls');
 
 /**
  * Expose `clickable`.
@@ -17,45 +18,22 @@ module.exports = clickable;
  */
 
 function clickable(el) {
-  
-  // find urls
-  
+  var pressed = Pressed();
   var html = el.innerHTML;
-  var urls = html
-    .split(/\s/)
-    .filter(function(word) {
-      return isUrl.test(word);
-    })
-    .map(function(url) {
-      while (/\.$/.test(url)) {
-        url = url.slice(0, -1);
-      }
-      return url;
-    });
   
-  // wrap urls in spans
-  
-  urls.forEach(function(url) {
+  findUrls(html).forEach(function(url) {
     var reg = new RegExp(escape(url), 'g');
     html = html.replace(reg, '<span class="command-click">' + url + '</span>');
   });
   el.innerHTML = html;
-  
-  // listen for clicks
-  
-  var pressed = Pressed();
-  
   el.addEventListener('click', function(e) {
     if (e.target.className == 'command-click' && pressed('meta')) {
       open(e.target.innerHTML);
     }
   });
   
-  // unbind
-  
   return function unbind() {
     removeEventListener('keydown', keydown);
     removeEventListener('keyup', keyup);
   };
-  
 }
